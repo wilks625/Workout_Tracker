a851e8490982a42e10eb1f7bd2a0ddc31e6e2fef
Stephen Yacas <syacas123@gmail.com>
2021-06-27 20:19:58 -0400

    modified server.js file

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require('morgan');
+const compression = require('compression');
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
+
+const db = require("./models");
 
 const app = express();
 
+app.use(logger("dev"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
+app.use(compression());
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
