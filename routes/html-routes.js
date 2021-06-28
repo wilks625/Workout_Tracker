1e30e47e4ec523ae0f7dcb77ffadab078fa6d664
Stephen Yacas <syacas123@gmail.com>
2021-06-27 22:34:46 -0400

    edited html routes for index.html, exercise.html, and stats.html

---
+++ routes/html-routes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const path = require('path');
+
+// index.html is rendered at endpoint /
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+// exercise.html is rendered at endpoint /exercise
+router.get('/exercise', (req, res) => {
+
+    res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+// stats.html is rendered at endpoint /stats
+router.get('/stats', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
+
+module.exports = router;
